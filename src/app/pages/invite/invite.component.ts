f7249cb9e9766bee8c09cba2a73e173e5b4e158e
diex <aboiledtiger@gmail.com>
2022-11-02 12:37:18 -0300

    dev: added minor css

--- src/app/pages/invite/invite.component.ts
+++ src/app/pages/invite/invite.component.ts
@@ -6,9 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./invite.component.scss'],
 })
 export class InviteComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
 
+  async share() {
+    const shareData = {
+      title: 'Out Of Order',
+      text: 'DEAR FRIEND COME ALONG LET’S GO ØUT ØF ØRDER',
+      url: 'https://outoforder-2022.web.app',
+    };
+
+    try {
+      await navigator.share(shareData);
+      console.log('MDN shared successfully');
+    } catch (err) {
+      console.error(`Error: ${err}`);
+      window.alert(`Can't share on this platform`);
+    }
+  }
 }
