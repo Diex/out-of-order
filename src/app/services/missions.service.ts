841ea2fa162e6f534d9d57e7bbdf5a9cea536d4d
diex <aboiledtiger@gmail.com>
2022-11-04 22:21:07 -0300

    dev: can update mission based on day and notification days

--- src/app/services/missions.service.ts
+++ src/app/services/missions.service.ts
@@ -2,60 +2,96 @@ import { Injectable } from '@angular/core';
 import { StorageService } from './localstorage.service';
 import * as missions from './../../assets/misions.json';
 import { BehaviorSubject } from 'rxjs';
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MissionsService {
   id = 0;
-  text = ""
-  
+  canUpdate = false;
+  // twice = [2, 5]
+
   current:BehaviorSubject<any> = new BehaviorSubject({});
 
   constructor(private store:StorageService) {    
    }
 
-  public next(){    
+  public async next(){    
+    
+    
 
-    this.store.get('mission').then((mission) => {    
-      if(!mission){
-        let date: Date = new Date();
-        let firstMission = {id: this.id, text: missions[this.id], shown:date};
-        this.store.set('mission', firstMission)              
-        this.current.next(firstMission); // muestro la actual
-        console.log('first mission created')      
-      }else{                
-        let date: Date = new Date();
-        this.id = (mission.id+1) % (Object.keys(missions).length-1);
-        this.text = mission.text;
-        let nextMission = {id: this.id, text: missions[this.id], shown:date};
-        this.store.set('mission', nextMission)      
-        this.current.next(nextMission);
-        console.log('get current mission...')      
-      }
+    await  this.store.get('store').then((store) => {    
+      let date: Date = new Date();
+      const m =  moment(date); // Thursday Feb 2015
+      const dow = m.day().toLocaleString();
+      console.log(store)    
+      this.canUpdate = this.matchDay(store.notifications, dow );
+      console.log(this.canUpdate)
     }).catch( () => {
       console.log('something went wrong')      
     });    
 
-    console.log(Object.keys(missions))
-    console.log(this.id, Object.keys(missions).length)
+    
+
+    // if matches selection
+    // if > 13:00
+
+
+    this.store.get('mission').then((mission) => {    
+      if(!mission){
+        this.createMission();
+      }else{                
+        if(!this.isToday(new Date(mission.shown)) && this.canUpdate){
+          this.updateMission(mission);
+        }else{
+          console.log('cant update same day');
+          this.current.next(mission);
+        }        
+      }
+
+      
+    }).catch( () => {
+      console.log('something went wrong')      
+    });    
+
+    // console.log(Object.keys(missions))
+    // console.log(this.id, Object.keys(missions).length)
     // this.updateMission();
   }
 
-  // updateMission(){
-  //   let date: Date = new Date();
-  //   this.store.get('mission').then((mission) => {    
-  //     if(!mission){
-  //       throw console.error('something went wrong');        
-  //     }else{
-  //       this.store.set('mission', {id: this.id, text: missions[this.id], shown:date})      
-  //       this.current.next(mission);
-  //     }
-  //   }).catch( () => {
-  //     console.log('no missions: create first')      
-  //   });
-    
+  createMission(){
+        let date: Date = new Date();        
+        let firstMission = {id: this.id, text: missions[this.id], shown:date};
+        this.store.set('mission', firstMission)              
+        this.current.next(firstMission); // muestro la actual
+        console.log('first mission created')      
+  }
 
-  // }
-  
+  updateMission(mission){
+        let date: Date = new Date();
+        this.id = (mission.id+1) % (Object.keys(missions).length-1);
+        let nextMission = {id: this.id, text: missions[this.id], shown:date};
+        this.store.set('mission', nextMission)      
+        this.current.next(nextMission);
+        console.log('get current mission...')      
+  }
+
+  matchDay(notification, dow){
+    if(!notification || notification === 'null') return false;
+    if(notification == 0) return true; // daily
+    if(notification == 1 && dow == 2) return true; // twice tuesday
+    if(notification == 1 && dow == 5) return true; // twice friday
+    if(notification == 2 && dow == 1) return true; // 3 monday
+    if(notification == 2 && dow == 3) return true; // 3 wendsday
+    if(notification == 2 && dow == 5) return true; // 3 friday
+    return false;
+  }
+
+  isToday(date){    
+      let m1 = moment(date);
+      let m2 = moment(new Date);
+      // console.log(m1, m2)
+      return m1.date() === m2.date() && m1.month() === m2.month()    
+  }
 }
